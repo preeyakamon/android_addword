4d2b830e24b7f342c597c475971aa6a9b703a018
preeyakamon <preeyakamon1515@gmail.com>
2017-06-17 18:38:37 +0700

    update

--- app/src/main/java/com/example/acer/addword/registerActivity.java
+++ app/src/main/java/com/example/acer/addword/registerActivity.java
@@ -1,6 +1,5 @@
 package com.example.acer.addword;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,37 +21,32 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class registerActivity extends AppCompatActivity {
 
-    EditText etusername, etpassword;
-    Button btnlogin, btnregister;
+    EditText etname, etusername, etpassword;
+    Button btnregister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
 
-        etusername = (EditText) findViewById(R.id.etusername);
-        etpassword = (EditText) findViewById(R.id.etpassword);
-        btnlogin = (Button) findViewById(R.id.btnlogin);
-        btnregister = (Button) findViewById(R.id.btnregister);
+        etname = (EditText)findViewById(R.id.etname);
+        etusername = (EditText)findViewById(R.id.etusername);
+        etpassword = (EditText)findViewById(R.id.etpassword);
+        btnregister = (Button)findViewById(R.id.btnregister);
 
         btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, registerActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btnlogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                String name = etname.getText().toString();
                 String username = etusername.getText().toString();
                 String password = etpassword.getText().toString();
-                String url = getString(R.string.ip_address) + "/addword/action/login.php";
+                String url = getString(R.string.ip_address) + "/addword/action/registeruser.php";
                 OkHttpClient client = new OkHttpClient();
                 RequestBody body = new FormBody.Builder()
+                        .add("name", name)
                         .add("username", username)
                         .add("password", password)
                         .build();
@@ -72,16 +66,21 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             JSONObject json = new JSONObject(formServer);
                             if (json.getBoolean("result")) { // if result = true
-                                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
-                                startActivity(intent);
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        onBackPressed();
+                                    }
+                                });
                             } else {
-                                Toast.makeText(MainActivity.this, "ข้อมูลผิดพลาด กรุณากรอกอีกครั้ง", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(registerActivity.this, "ข้อมูลผิดพลาด กรุณากรอกอีกครั้ง", Toast.LENGTH_SHORT).show();
                             }
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 });
+
             }
         });
     }
